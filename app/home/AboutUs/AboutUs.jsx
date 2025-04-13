a8f757b56c891a52bdd07f35c2e8ae3321b8da03
joySutradhor <joysutradhorcmt@gmail.com>
2025-04-13 13:55:32 +0600

    sytles added

--- app/home/AboutUs/AboutUs.jsx
+++ app/home/AboutUs/AboutUs.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
-import "./aboutUs.css"
+import './aboutUs.css'
 
 function AboutUs () {
-  return <div className='dummy'>This is about us</div>
+  return (
+    <div className='bg-gray-300 px-[20vw] py-[5vh]'>
+      <div className='largeText'>tis is testing div</div>
+      <div>tis is testing two</div>
+      <div>tis is testing there</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div className='largeText'>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div className='largeText'>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div className='largeText'>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div>tis is testing four</div>
+      <div className='text-7xl text-red-700'>tis is testing five</div>
+    </div>
+  )
 }
 
 export default AboutUs
