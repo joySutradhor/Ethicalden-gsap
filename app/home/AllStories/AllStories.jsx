ff9aa02c13ceb2cc9060b8e12f0850390f104b77
joySutradhor <joysutradhorcmt@gmail.com>
2025-04-17 13:02:02 +0600

    nothing

--- app/home/AllStories/AllStories.jsx
+++ app/home/AllStories/AllStories.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 function AllStories () {
-  return <div>AllStories</div>
+  return <section>
+    <h1>heloo</h1>
+  </section>
 }
 
 export default AllStories
---
+++ components/AllStories/AllStories.jsx
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+
+function AllStories() {
+    // const  storiesData = axios.get()
+  return (
+    <div>AllStories</div>
+  )
+}
+
+export default AllStories
