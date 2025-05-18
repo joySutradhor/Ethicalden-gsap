a2dc945ba2316f68bd71678ba1ab77a268cde9d1
mahfuj926 <mahfujrahman.jewel@gmail.com>
2025-05-18 11:38:45 +0600

    Added some modern text to appear randomly in loader

--- components/Loader/Loader.jsx
+++ components/Loader/Loader.jsx
@@ -1,21 +1,35 @@
 'use client'
 
 import gsap from 'gsap'
-import React, { useEffect, useMemo } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Loader = ({ className = '' }) => {
-  // Get current day in full format
-  const currentDay = useMemo(() => {
-    return new Date().toLocaleDateString('en-US', { weekday: 'long' })
+  const [randomText, setRandomText] = useState(null)
+
+  const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' })
+
+  const texts = [
+    "Let's build something great.",
+    'Innovation starts today.',
+    'Deliver with purpose.',
+    'Design. Code. Repeat.',
+    'Pushing boundaries forward.',
+    'Execute with excellence.',
+    'Modern problems, smart solutions.',
+    'Crafting clean experiences.',
+    'Stay sharp. Stay efficient.',
+    'Where ideas meet execution.'
+  ]
+
+  // random text 
+  useEffect(() => {
+    const index = Math.floor(Math.random() * texts.length)
+    setRandomText(texts[index])
   }, [])
 
-  // Words to display in the loader
-  const words = useMemo(() => {
-    return [`${currentDay}.`, 'The', 'speed', 'bump', 'of', 'the', 'week']
-  }, [currentDay])
-
-  // GSAP animation for character reveal
+  // Animate after text is available
   useEffect(() => {
+    if (!randomText) return
     const animation = gsap.fromTo(
       '.char',
       { color: '#808080' },
@@ -29,19 +43,20 @@ const Loader = ({ className = '' }) => {
         ease: 'power2.out'
       }
     )
+    return () => animation.kill()
+  }, [randomText])
 
-    return () => {
-      animation.kill()
-    }
-  }, [])
+  if (!randomText) return null 
+
+  const words = [`${currentDay}.`, ...randomText.split(' ')]
 
   return (
     <section className='h-[100vh] w-full bg-[#EBECE7] fixed top-0 flex justify-center items-center px-10 md:px-12 lg:px-16 xl:px-20 z-50'>
       <div
-        className={`flex flex-wrap gap-2 md:gap-3 text-gray-400 text-[42px] md:text-[50px] lg:text-[60px] xl:text-[80px]  2xl:text-[90px] font-urbanist font-black leading-[1.06] py-8 md:py-30 ${className}`}
+        className={`flex flex-wrap gap-2 md:gap-3 text-gray-400 text-[42px] md:text-[50px] lg:text-[60px] xl:text-[80px] 2xl:text-[90px] font-urbanist font-black leading-[1.06] py-8 md:py-30 ${className}`}
       >
         {words.map((word, index) => (
-          <div className='word flex' key={`word-${index}`}>
+          <div className='word flex mr-2' key={`word-${index}`}>
             {word.split('').map((char, i) => (
               <span
                 className='char'
