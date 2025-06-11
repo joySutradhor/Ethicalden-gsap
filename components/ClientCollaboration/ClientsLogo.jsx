10bac0855fc55c9b4c8c030e255c89ecf60d3281
mahfuj926 <mahfujrahman.jewel@gmail.com>
2025-06-11 16:21:12 +0600

    Added client logo

--- components/ClientCollaboration/ClientsLogo.jsx
+++ components/ClientCollaboration/ClientsLogo.jsx
@@ -4,57 +4,62 @@ function ClientsLogo() {
   const items = [
     {
       id: 1,
-      src: 'https://ik.imagekit.io/ethicalden/logo/agarwal.webp?updatedAt=1747073875209',
+      src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-1.webp?updatedAt=1749636411399',
       line: 'yes'
     },
     {
       id: 2,
-      src: 'https://ik.imagekit.io/ethicalden/logo/brandghar.webp?updatedAt=1747073875151',
+      src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-2.webp?updatedAt=1749636418967',
       line: 'yes'
     },
     {
       id: 3,
-      src: 'https://ik.imagekit.io/ethicalden/logo/emopract.webp?updatedAt=1747073870701',
+      src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-3.webp?updatedAt=1749636424122',
       line: 'yes'
     },
     {
       id: 4,
-      src: 'https://ik.imagekit.io/ethicalden/logo/griham.webp?updatedAt=1747073870640',
+      src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-4.webp?updatedAt=1749636431592',
       line: 'yes'
     },
     {
       id: 5,
-      src: 'https://ik.imagekit.io/ethicalden/logo/hero.webp?updatedAt=1747073870371',
+      src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-5.webp?updatedAt=1749636439438',
       line: 'yes'
     },
     {
       id: 6,
-      src: 'https://ik.imagekit.io/ethicalden/logo/massart.png?updatedAt=1747073870731',
+      src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-6.webp?updatedAt=1749636458671',
       line: 'yes'
     },
     {
       id: 7,
-      src: 'https://ik.imagekit.io/ethicalden/logo/ridhisidhi.webp?updatedAt=1747073870637',
+      src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-7.webp?updatedAt=1749636464683',
       line: 'yes'
     },
     {
       id: 8,
-      src: 'https://ik.imagekit.io/ethicalden/logo/Siliguri_Metropolitan_Police_Logo.png?updatedAt=1747073870735',
+      src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-8.webp?updatedAt=1749636470191',
       line: 'yes'
     },
     {
       id: 9,
-      src: 'https://ik.imagekit.io/ethicalden/logo/smit.webp?updatedAt=1747073870739',
+      src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-9.webp?updatedAt=1749636475545',
       line: 'yes'
     },
     {
       id: 10,
-      src: 'https://ik.imagekit.io/ethicalden/logo/tala%20pottay.webp?updatedAt=1747073874032',
+      src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-10.webp?updatedAt=1749636481915',
       line: 'yes'
     },
     {
       id: 11,
-      src: '/images/clients-logo/logo/UNESCO_logo.svg',
+      src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-11.webp?updatedAt=1749636486824',
+      line: 'yes'
+    },
+    {
+      id: 12,
+      src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-12.webp?updatedAt=1749636493578',
       line: 'yes'
     },
 
@@ -63,20 +68,20 @@ function ClientsLogo() {
 
   return (
     <div className='px-5 xl:px-20 2xl:px-50'>
-      <div className='grid grid-cols-3 lg:grid-cols-6 gap-4'>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4'>
         {items.map(item => (
           <div
             key={item.id}
-            className='flex items-center justify-center rounded bg-white p-2 h-[150px]'
+            className='flex items-center justify-center rounded bg-white p-2 h-[180px]'
           >
             <div className='relative w-full flex items-center justify-center '>
               <Image
                 src={item.src}
+                loading='lazy'
                 alt={`Item ${item.id}`}
                 width={100}
                 height={100}
-                unoptimized
-                className='object-cover w-auto h-[120px] '
+                className='object-cover w-full h-[120px] md:h-[180px] '
               />
             </div>
             {item.line === 'yes' && (
