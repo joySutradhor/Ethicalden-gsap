c7c7c1fd3ca286e167d55dd939724947c741d452
mahfuj926 <mahfujrahman.jewel@gmail.com>
2025-06-16 13:53:39 +0600

    recheck

--- components/Gallery/gallery.jsx
+++ components/Gallery/gallery.jsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 
 gsap.registerPlugin(ScrollTrigger)
 
-const GalleryMain = async () => {
+const GalleryMain = () => {
   const sectionRef = useRef()
   const wrapperRef = useRef(null)
   const videoRef = useRef(null)
