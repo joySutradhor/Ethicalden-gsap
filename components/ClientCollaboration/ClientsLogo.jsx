73b4a170faa1d6d926a7bae06f025da8f9cc287f
mahfuj926 <mahfujrahman.jewel@gmail.com>
2025-06-18 13:25:52 +0600

    again checking

--- components/ClientCollaboration/ClientsLogo.jsx
+++ components/ClientCollaboration/ClientsLogo.jsx
@@ -1,10 +1,10 @@
 'use client'
-import Image from 'next/image'
+import { useState } from 'react';
+import Image from 'next/image';
 import { blurPlaceholder } from '../utils/blur-placeholder';
 
 function ClientsLogo() {
   const items = [
-    
     {
       id: 1,
       src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-2.webp?updatedAt=1749636418967',
@@ -65,19 +65,38 @@ function ClientsLogo() {
       src: 'https://ik.imagekit.io/ckncpdy03/Ethical%20den%20-%20gsap/Home%20Page/logo-1.webp?updatedAt=1749636411399',
       line: 'yes'
     },
-
   ];
 
+  const [loadingStates, setLoadingStates] = useState(
+    Array(items.length).fill(true)
+  );
+
+  const handleImageLoad = (index) => {
+    setLoadingStates((prev) => {
+      const updated = [...prev];
+      updated[index] = false;
+      return updated;
+    });
+  };
 
   return (
     <div className='px-5 xl:px-20 2xl:px-50'>
       <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4'>
-        {items.map(item => (
+        {items.map((item, index) => (
           <div
             key={item.id}
             className='flex items-center justify-center rounded bg-white p-2 h-[180px]'
           >
-            <div className='relative w-full flex items-center justify-center '>
+            <div className='relative w-full flex items-center justify-center'>
+
+              {/* Spinner overlay */}
+              {loadingStates[index] && (
+                <div className="absolute inset-0 flex items-center justify-center z-90 bg-white/40 backdrop-blur-sm rounded">
+                  <div className="w-10 h-10 border-4 border-black border-t-transparent rounded-full animate-spin"></div>
+                </div>
+              )}
+
+              {/* Image */}
               <Image
                 src={item.src}
                 loading='lazy'
@@ -86,9 +105,11 @@ function ClientsLogo() {
                 height={500}
                 placeholder='blur'
                 blurDataURL={blurPlaceholder}
-                className='object-cover w-full h-[120px] md:h-[180px] '
+                className='object-contain w-full h-[120px] md:h-[180px]'
+                onLoad={() => handleImageLoad(index)}
               />
             </div>
+
             {item.line === 'yes' && (
               <div className='h-[70px] w-[1px] bg-black/20 rotate-12 ml-5'></div>
             )}
@@ -96,7 +117,7 @@ function ClientsLogo() {
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default ClientsLogo
+export default ClientsLogo;
