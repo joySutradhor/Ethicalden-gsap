215f25343f4159b44c07488385a36d8a8defad88
mahfuj926 <mahfujrahman.jewel@gmail.com>
2025-06-23 10:58:12 +0600

    Added our works page

--- components/pages/OurWorks/OurWorksHome.jsx
+++ components/pages/OurWorks/OurWorksHome.jsx
@@ -1,14 +1,14 @@
 import Navbar from '@/components/Shared/Navbar/Navbar';
 import React from 'react';
-import OurWorksArea from './OurWorksArea';
 import Footer from '@/components/Shared/Footer/Footer';
+import OurWorksArea2 from './OurWorksArea2';
 
 const OurWorksHome = () => {
     return (
         <div className="bg-[#111] text-white ">
             <Navbar backgroundColor="#111" textColor="white" />
             <div>
-                <OurWorksArea />
+                <OurWorksArea2 />
             </div>
             <Footer />
         </div>
