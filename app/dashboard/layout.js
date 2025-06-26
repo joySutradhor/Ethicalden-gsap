35e1b67475dedb5854704dccdb7e4fc111488448
joySutradhor <joysutradhorcmt@gmail.com>
2025-06-26 22:37:51 +0600

    add manage request funcitonality , accept , reject and deleted

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,19 +1,21 @@
+"use client"
 import './dashboard.css'
-import Sidebar from './dashboardComponent/sidebar'
-
+import useAuthRedirect from './dashboardComponent/hooks/useAuthRedirect'
+import SidebarMain from './dashboardComponent/Sidebar/SidebarMain'
 
 // This layout overrides the root layout
-export const metadata = {
-  title: 'Dashboard | Barcelona Market Card',
-  description: 'Private admin section'
-}
+// export const metadata = {
+//   title: 'Dashboard | Barcelona Market Card',
+//   description: 'Private admin section'
+// }
 
 export default function DashboardLayout ({ children }) {
+  useAuthRedirect()
   return (
     <html lang='en' data-arp=''>
       <body cz-shortcut-listen='true'>
         <div className='grid lg:grid-cols-[20%_80%] 2xl:grid-cols-[15%_85%] justify-between min-h-screen bg-[#131313]'>
-          <Sidebar />
+          <SidebarMain />
           <main>{children}</main>
         </div>
       </body>
